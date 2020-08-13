c4bbf3d94c9bdf78299e75a872885766777c82e4
wikomatic <matthew.wikoff@gmail.com>
2020-08-12 23:11:24 -0400

    Berry trees no longer disappear. From https://www.pokecommunity.com/showthread.php?p=10142996#post10142996

--- src/berry.c
+++ src/berry.c
@@ -1420,16 +1420,6 @@ static bool32 BerryTreeGrow(struct BerryTree *tree)
     case BERRY_STAGE_TALLER:
         tree->stage++;
         break;
-    case BERRY_STAGE_BERRIES:
-        tree->watered1 = 0;
-        tree->watered2 = 0;
-        tree->watered3 = 0;
-        tree->watered4 = 0;
-        tree->berryYield = 0;
-        tree->stage = BERRY_STAGE_SPROUTED;
-        if (++tree->regrowthCount == 10)
-            *tree = gBlankBerryTree;
-        break;
     }
     return TRUE;
 }
@@ -1443,7 +1433,7 @@ void BerryTreeTimeUpdate(s32 minutes)
     {
         tree = &gSaveBlock1Ptr->berryTrees[i];
 
-        if (tree->berry && tree->stage && !tree->growthSparkle)
+        if (tree->berry && tree->stage && !tree->growthSparkle && (tree->stage != BERRY_STAGE_BERRIES))
         {
             if (minutes >= GetStageDurationByBerryType(tree->berry) * 71)
             {
